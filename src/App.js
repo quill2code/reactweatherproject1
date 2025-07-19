a172cbbf84d84b29c9e68451d2a0d03c1b55bafc
quill2code <denisehjohnson@yahoo.com>
2025-07-19 14:05:27 -0700

    Update App.js
    add city

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+ximport React from "react";
 import Weather from "./Weather";
 import "./App.css";
 
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="San Francisco" />
+        <Weather defaultCity="Phoenix" />
       </div>
     </div>
   );
